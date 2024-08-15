16b6dd58ef2889c95161242dae64598e6dc9603e
Matibox <mateusz.hladky@gmail.com>
2024-08-15 17:20:47 +0200

    add teams to schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -28,13 +28,13 @@ export const posts = createTable(
       .default(sql`(unixepoch())`)
       .notNull(),
     updatedAt: int("updatedAt", { mode: "timestamp" }).$onUpdate(
-      () => new Date()
+      () => new Date(),
     ),
   },
   (example) => ({
     createdByIdIdx: index("created_by_idx").on(example.createdById),
     nameIndex: index("name_idx").on(example.name),
-  })
+  }),
 );
 
 export const users = createTable("user", {
@@ -43,6 +43,8 @@ export const users = createTable("user", {
     .primaryKey()
     .$defaultFn(() => crypto.randomUUID()),
   name: text("name", { length: 255 }),
+  firstName: text("first_name", { length: 255 }),
+  lastName: text("last_name", { length: 255 }),
   email: text("email", { length: 255 }).notNull(),
   emailVerified: int("email_verified", {
     mode: "timestamp",
@@ -53,6 +55,21 @@ export const users = createTable("user", {
 export const usersRelations = relations(users, ({ many }) => ({
   accounts: many(accounts),
 }));
+
+export const teams = createTable("team", {
+  id: int("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  name: text("name", { length: 255 }),
+});
+
+export const usersToTeams = createTable("users_to_teams", {
+  userId: int("user_id")
+    .notNull()
+    .references(() => users.id),
+  teamId: int("team_id")
+    .notNull()
+    .references(() => users.id),
+  shirtNumber: int("shirt_number", { mode: "number" }),
+});
 
 export const accounts = createTable(
   "account",
@@ -78,7 +95,7 @@ export const accounts = createTable(
       columns: [account.provider, account.providerAccountId],
     }),
     userIdIdx: index("account_user_id_idx").on(account.userId),
-  })
+  }),
 );
 
 export const accountsRelations = relations(accounts, ({ one }) => ({
@@ -96,7 +113,7 @@ export const sessions = createTable(
   },
   (session) => ({
     userIdIdx: index("session_userId_idx").on(session.userId),
-  })
+  }),
 );
 
 export const sessionsRelations = relations(sessions, ({ one }) => ({
@@ -112,5 +129,5 @@ export const verificationTokens = createTable(
   },
   (vt) => ({
     compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
-  })
+  }),
 );
