1a2ea85c2d81b36af5c5ea6f83aebf62e489cadb
Matibox <mateusz.hladky@gmail.com>
2024-08-17 01:11:21 +0200

    List placeholder teams

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,32 @@
+import Link from "next/link";
 import { redirect } from "next/navigation";
+import { Button } from "~/components/ui/button";
 import { getServerAuthSession } from "~/server/auth";
+
+const teams = [{ name: "Sparta Skoczów" }, { name: "Szybin" }].map(
+  (team, i) => ({ id: i, ...team }),
+);
 
 export default async function Dashboard() {
   const session = await getServerAuthSession();
 
   if (!session) redirect("/");
 
-  return <div>dashboard</div>;
+  return (
+    <main className="flex min-h-screen flex-col items-center gap-8 px-4 py-8">
+      <h1 className="text-3xl font-bold leading-none">Drużyny</h1>
+      <Button>Nowa drużyna</Button>
+      <div className="grid w-full grid-cols-[repeat(auto-fill,_343px)] justify-center gap-4">
+        {teams.map((team) => (
+          <Link
+            key={team.id}
+            href={`/${team.id}`}
+            className="w-full max-w-96 rounded-md bg-muted p-4 transition-colors hover:bg-muted/85"
+          >
+            <span className="font-semibold leading-none">{team.name}</span>
+          </Link>
+        ))}
+      </div>
+    </main>
+  );
 }
