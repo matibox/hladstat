e07299ed065aa436bc1065de0e7e5afe62a11ae1
Matibox <mateusz.hladky@gmail.com>
2024-08-19 00:15:09 +0200

    Add matches table

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -17,6 +17,71 @@ import type { roles } from "~/lib/constants";
  */
 export const createTable = sqliteTableCreator((name) => `hladstat_${name}`);
 
+// ==== next auth ====
+
+export const accounts = createTable(
+  "account",
+  {
+    userId: text("user_id", { length: 255 })
+      .notNull()
+      .references(() => users.id),
+    type: text("type", { length: 255 })
+      .$type<AdapterAccount["type"]>()
+      .notNull(),
+    provider: text("provider", { length: 255 }).notNull(),
+    providerAccountId: text("provider_account_id", { length: 255 }).notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: int("expires_at"),
+    token_type: text("token_type", { length: 255 }),
+    scope: text("scope", { length: 255 }),
+    id_token: text("id_token"),
+    session_state: text("session_state", { length: 255 }),
+  },
+  (account) => ({
+    compoundKey: primaryKey({
+      columns: [account.provider, account.providerAccountId],
+    }),
+    userIdIdx: index("account_user_id_idx").on(account.userId),
+  }),
+);
+
+export const accountsRelations = relations(accounts, ({ one }) => ({
+  user: one(users, { fields: [accounts.userId], references: [users.id] }),
+}));
+
+export const sessions = createTable(
+  "session",
+  {
+    sessionToken: text("session_token", { length: 255 }).notNull().primaryKey(),
+    userId: text("userId", { length: 255 })
+      .notNull()
+      .references(() => users.id),
+    expires: int("expires", { mode: "timestamp" }).notNull(),
+  },
+  (session) => ({
+    userIdIdx: index("session_userId_idx").on(session.userId),
+  }),
+);
+
+export const sessionsRelations = relations(sessions, ({ one }) => ({
+  user: one(users, { fields: [sessions.userId], references: [users.id] }),
+}));
+
+export const verificationTokens = createTable(
+  "verification_token",
+  {
+    identifier: text("identifier", { length: 255 }).notNull(),
+    token: text("token", { length: 255 }).notNull(),
+    expires: int("expires", { mode: "timestamp" }).notNull(),
+  },
+  (vt) => ({
+    compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
+  }),
+);
+
+// ====
+
 export const users = createTable("user", {
   id: text("id", { length: 255 })
     .notNull()
@@ -60,72 +125,23 @@ export const usersToTeams = createTable(
   }),
 );
 
-export const accounts = createTable(
-  "account",
-  {
-    userId: text("user_id", { length: 255 })
-      .notNull()
-      .references(() => users.id),
-    type: text("type", { length: 255 })
-      .$type<AdapterAccount["type"]>()
-      .notNull(),
-    provider: text("provider", { length: 255 }).notNull(),
-    providerAccountId: text("provider_account_id", { length: 255 }).notNull(),
-    refresh_token: text("refresh_token"),
-    access_token: text("access_token"),
-    expires_at: int("expires_at"),
-    token_type: text("token_type", { length: 255 }),
-    scope: text("scope", { length: 255 }),
-    id_token: text("id_token"),
-    session_state: text("session_state", { length: 255 }),
-  },
-  (account) => ({
-    compoundKey: primaryKey({
-      columns: [account.provider, account.providerAccountId],
-    }),
-    userIdIdx: index("account_user_id_idx").on(account.userId),
-  }),
-);
-
-export const sessions = createTable(
-  "session",
-  {
-    sessionToken: text("session_token", { length: 255 }).notNull().primaryKey(),
-    userId: text("userId", { length: 255 })
-      .notNull()
-      .references(() => users.id),
-    expires: int("expires", { mode: "timestamp" }).notNull(),
-  },
-  (session) => ({
-    userIdIdx: index("session_userId_idx").on(session.userId),
-  }),
-);
-
-export const verificationTokens = createTable(
-  "verification_token",
-  {
-    identifier: text("identifier", { length: 255 }).notNull(),
-    token: text("token", { length: 255 }).notNull(),
-    expires: int("expires", { mode: "timestamp" }).notNull(),
-  },
-  (vt) => ({
-    compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
-  }),
-);
+export const matches = createTable("matches", {
+  id: int("id", { mode: "number" })
+    .primaryKey({ autoIncrement: true })
+    .notNull(),
+  teamId: int("team_id")
+    .notNull()
+    .references(() => teams.id),
+  date: int("date", { mode: "timestamp" }).notNull(),
+  opponent: text("opponent", { length: 255 }).notNull(),
+  score: text("score", { length: 3 }).notNull(),
+});
 
 // ==== relations ====
 
 export const usersRelations = relations(users, ({ many }) => ({
   accounts: many(accounts),
   teams: many(usersToTeams),
-}));
-
-export const accountsRelations = relations(accounts, ({ one }) => ({
-  user: one(users, { fields: [accounts.userId], references: [users.id] }),
-}));
-
-export const sessionsRelations = relations(sessions, ({ one }) => ({
-  user: one(users, { fields: [sessions.userId], references: [users.id] }),
 }));
 
 export const usersToTeamsRelations = relations(usersToTeams, ({ one }) => ({
@@ -135,4 +151,9 @@ export const usersToTeamsRelations = relations(usersToTeams, ({ one }) => ({
 
 export const teamRelations = relations(teams, ({ many }) => ({
   users: many(usersToTeams),
+  matches: many(matches),
+}));
+
+export const matchesRelations = relations(matches, ({ one }) => ({
+  team: one(teams, { fields: [matches.teamId], references: [teams.id] }),
 }));
