1f3fe247412655b9b49f004e05bec28aede32805
Matibox <mateusz.hladky@gmail.com>
2024-08-28 17:23:36 +0200

    Fix /dashboard/[teamId] max-w

--- src/app/dashboard/[teamId]/page.tsx
+++ src/app/dashboard/[teamId]/page.tsx
@@ -20,7 +20,7 @@ export default async function Team({
 
   return (
     <HydrateClient>
-      <main className="mx-auto flex w-full max-w-xl flex-col gap-8 px-4 pb-4 md:grid md:max-w-5xl md:grid-cols-2 md:px-6 md:pb-6 lg:gap-12 lg:px-8 lg:pb-8">
+      <main className="mx-auto flex w-full max-w-xl flex-col gap-8 px-4 pb-4 md:grid md:max-w-7xl md:grid-cols-2 md:px-6 md:pb-6 lg:gap-12 lg:px-8 lg:pb-8">
         {/* matches */}
         <section className="flex w-full flex-col gap-4">
           <div className="flex w-full items-center justify-between">
