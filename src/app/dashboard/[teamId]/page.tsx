327d46cad67366d4a7646ae28129220fa4978d6d
Matibox <mateusz.hladky@gmail.com>
2024-09-14 22:49:31 +0200

    Add shared access people list

--- src/app/dashboard/[teamId]/page.tsx
+++ src/app/dashboard/[teamId]/page.tsx
@@ -1,3 +1,4 @@
+import { Share2Icon, TrashIcon } from "lucide-react";
 import AddPlayerForm from "~/components/AddPlayerForm";
 import MatchCards from "~/components/MatchCards";
 import NewMatchForm from "~/components/NewMatchForm";
@@ -9,9 +10,23 @@ import {
   ServeChart,
   SetDistributionChart,
 } from "~/components/StatsCharts";
+import TeamPageNavbar from "~/components/TeamPageNavbar";
+import { Button } from "~/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
+import { TabsContent } from "~/components/ui/tabs";
 import { countPointsAndErrors, countStat } from "~/lib/stats";
 import { api, HydrateClient } from "~/trpc/server";
+
+const PLACEHOLDER_PLAYERS = [
+  {
+    id: 1,
+    name: "Jan Kowalski",
+  },
+  {
+    id: 2,
+    name: "Adam Nowak",
+  },
+];
 
 export default async function Team({
   params: { teamId: _teamId },
@@ -29,60 +44,97 @@ export default async function Team({
 
   return (
     <HydrateClient>
-      <main className="mx-auto flex w-full max-w-xl flex-col gap-8 px-4 pb-4 md:grid md:max-w-7xl md:grid-cols-2 md:px-6 md:pb-6 lg:gap-12 lg:px-8 lg:pb-8">
-        {/* matches */}
-        <section className="flex w-full flex-col gap-4">
-          <div className="flex w-full items-center justify-between">
-            <h1 className="text-2xl font-semibold">Ostatnie mecze</h1>
-            <NewMatchForm teamId={teamId} />
-          </div>
-          <MatchCards teamId={teamId} />
-        </section>
-        {/* players */}
-        <section className="flex w-full flex-col gap-4">
-          <div className="flex w-full items-center justify-between">
-            <h2 className="text-2xl font-semibold">Zawodnicy</h2>
-            <AddPlayerForm teamId={teamId} />
-          </div>
-          <TeamPlayers teamId={teamId} />
-        </section>
-        {/* stats */}
-        <section className="flex w-full flex-col gap-4 md:col-span-2">
-          <h2 className="text-2xl font-semibold">Statystyki drużyny</h2>
-          <div className="flex flex-col gap-4 md:grid md:grid-cols-2">
-            <SetDistributionChart stats={stats} />
-            <ScorersChart stats={stats} />
-            <AttackChart stats={stats} />
-            <ReceptionChart stats={stats} />
-            <ServeChart stats={stats} />
-            <Card className="border-none bg-muted/25">
-              <CardHeader className="p-4">
-                <CardTitle className="text-xl leading-none">
-                  Inne statystyki
-                </CardTitle>
-              </CardHeader>
-              <CardContent className="p-4 pt-0">
-                <div className="flex gap-16">
-                  <div className="flex flex-col text-muted-foreground">
-                    <span>Punkty</span>
-                    <span>Błędy</span>
-                    <span>Bloki</span>
-                    <span>Obrony</span>
+      <TeamPageNavbar>
+        <main className="mx-auto flex w-full max-w-xl flex-col gap-8 px-4 pb-[calc(75px_+_1rem)] md:grid md:max-w-7xl md:grid-cols-2 md:px-6 md:pb-6 lg:gap-12 lg:px-8 lg:pb-8">
+          <TabsContent value="matches">
+            <section className="flex w-full flex-col gap-4">
+              <div className="flex w-full items-center justify-between">
+                <h1 className="text-2xl font-semibold">Ostatnie mecze</h1>
+                <NewMatchForm teamId={teamId} />
+              </div>
+              <MatchCards teamId={teamId} />
+            </section>
+          </TabsContent>
+          <TabsContent value="members">
+            <section className="flex w-full flex-col gap-4">
+              <div className="flex w-full items-center justify-between">
+                <h2 className="text-2xl font-semibold">Zawodnicy</h2>
+                <AddPlayerForm teamId={teamId} />
+              </div>
+              <TeamPlayers teamId={teamId} />
+            </section>
+          </TabsContent>
+          <TabsContent value="stats">
+            <section className="flex w-full flex-col gap-4 md:col-span-2">
+              <div className="flex flex-col gap-4 md:grid md:grid-cols-2">
+                <SetDistributionChart stats={stats} />
+                <ScorersChart stats={stats} />
+                <AttackChart stats={stats} />
+                <ReceptionChart stats={stats} />
+                <ServeChart stats={stats} />
+                <Card className="border-none bg-muted/25">
+                  <CardHeader className="p-4">
+                    <CardTitle className="text-xl leading-none">
+                      Inne statystyki
+                    </CardTitle>
+                  </CardHeader>
+                  <CardContent className="p-4 pt-0">
+                    <div className="flex gap-16">
+                      <div className="flex flex-col text-muted-foreground">
+                        <span>Punkty</span>
+                        <span>Błędy</span>
+                        <span>Bloki</span>
+                        <span>Obrony</span>
+                      </div>
+                      <div className="flex flex-col">
+                        <span>{points}</span>
+                        <span>{errors}</span>
+                        <span>{countStat(stats, ["other-blk"])}</span>
+                        <span>{countStat(stats, ["other-dig"])}</span>
+                      </div>
+                    </div>
+                  </CardContent>
+                </Card>
+              </div>
+            </section>
+          </TabsContent>
+          <TabsContent value="settings">
+            <section className="flex w-full flex-col gap-4">
+              <div className="flex flex-col gap-2">
+                <h2 className="text-xl font-semibold leading-none">
+                  Udostępniony dostęp
+                </h2>
+                <p className="text-sm text-muted-foreground">
+                  Lista osób, które mogą zobaczyć statystyki meczów oraz
+                  zawodników.
+                </p>
+              </div>
+              <div className="flex flex-col gap-1">
+                {PLACEHOLDER_PLAYERS.map((player) => (
+                  <div key={player.id} className="flex items-center gap-2">
+                    <div className="h-6 w-px bg-muted" />
+                    <div className="flex flex-col gap-0.5">
+                      <span className="text-sm font-medium leading-none">
+                        {player.name}
+                      </span>
+                    </div>
+                    <Button
+                      variant="ghost"
+                      className="ml-auto h-auto w-auto p-2"
+                    >
+                      <TrashIcon className="h-4 w-4 text-red-500" />
+                    </Button>
                   </div>
-                  <div className="flex flex-col">
-                    <span>{points}</span>
-                    <span>{errors}</span>
-                    <span>{countStat(stats, ["other-blk"])}</span>
-                    <span>{countStat(stats, ["other-dig"])}</span>
-                  </div>
-                </div>
-              </CardContent>
-            </Card>
-          </div>
-        </section>
-        {/* settings/danger zone */}
-        {/* <section></section> */}
-      </main>
+                ))}
+              </div>
+              <Button className="self-start" size="sm">
+                <span>Dodaj osoby</span>
+                <Share2Icon className="ml-1.5 h-4 w-4" />
+              </Button>
+            </section>
+          </TabsContent>
+        </main>
+      </TeamPageNavbar>
     </HydrateClient>
   );
 }
---
+++ src/components/TeamPageNavbar.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import Link from "next/link";
+import { Tabs, TabsList, TabsTrigger } from "./ui/tabs";
+import {
+  BarChartIcon,
+  SettingsIcon,
+  SwordsIcon,
+  UsersIcon,
+} from "lucide-react";
+import { usePathname, useSearchParams } from "next/navigation";
+
+export default function TeamPageNavbar({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  return (
+    <Tabs value={searchParams.get("t") ?? "matches"}>
+      {children}
+      <div className="fixed bottom-0 left-0 right-0 border-t border-border bg-background py-2 md:hidden">
+        <TabsList className="grid h-auto w-full grid-cols-4 bg-background">
+          <TabsTrigger value="matches" asChild>
+            <Link
+              href={`${pathname}?t=matches`}
+              className="flex flex-col gap-1"
+              shallow
+            >
+              <SwordsIcon className="h-5 w-5" />
+              <span className="text-sm leading-none">Mecze</span>
+            </Link>
+          </TabsTrigger>
+          <TabsTrigger value="members" asChild>
+            <Link
+              href={`${pathname}?t=members`}
+              className="flex flex-col gap-1"
+              shallow
+            >
+              <UsersIcon className="h-5 w-5" />
+              <span className="text-sm leading-none">Członkowie</span>
+            </Link>
+          </TabsTrigger>
+          <TabsTrigger value="stats" asChild>
+            <Link
+              href={`${pathname}?t=stats`}
+              className="flex flex-col gap-1"
+              shallow
+            >
+              <BarChartIcon className="h-5 w-5" />
+              <span className="text-sm leading-none">Statystyki</span>
+            </Link>
+          </TabsTrigger>
+          <TabsTrigger value="settings" asChild>
+            <Link
+              href={`${pathname}?t=settings`}
+              className="flex flex-col gap-1"
+              shallow
+            >
+              <SettingsIcon className="h-5 w-5" />
+              <span className="text-sm leading-none">Ustawienia</span>
+            </Link>
+          </TabsTrigger>
+        </TabsList>
+      </div>
+    </Tabs>
+  );
+}
